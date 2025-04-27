113f15f4b059557bde31edfc9b092ba07ca5aa94
yaumianwar <nuryaumi10@gmail.com>
2025-04-28 04:53:12 +0800

    add data validation on the extract task to check if the data is newest version or not

--- dags/etl_forecast.py
+++ dags/etl_forecast.py
@@ -1,10 +1,15 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from helpers.db_connect import get_clickhouse_connection
 from helpers.get_forecasts import get_forecasts
 from airflow.decorators import dag, task
+from airflow.exceptions import AirflowException
+from airflow.operators.python_operator import PythonOperator
 
 FORECAST_DB_COLUMNS = ['location_code', 'timezone', 'forecast_code', 'datetime', 't', 'tcc', 'tp', 'weather', 'weather_desc', 'weather_desc_en', 'wd_deg', 'wd', 'wd_to', 'ws', 'hu', 'vs', 'vs_text', 'time_index', 'analysis_date', 'image', 'utc_datetime', 'local_datetime']
 DATETIME_KEYS = ['datetime', 'analysis_date', 'utc_datetime', 'local_datetime']
+
+# get clickhouse connection
+client = get_clickhouse_connection()
 
 # Function to convert string to datetime
 def parse_datetime(datetime_str):
@@ -17,26 +22,54 @@ def parse_datetime(datetime_str):
             continue
     raise ValueError(f"Unable to parse datetime: {datetime_str}")
 
+def is_newest_version(new_analysis_date):
+    # get the latest analysis date from existing data
+    query_result = client.query('select max(analysis_date) from forecasts')
+    current_analysis_date = query_result.result_rows[0][0]
+
+    print(f'current_analysis_date {current_analysis_date}. new_analysis_date {new_analysis_date}')
+
+    if current_analysis_date >= datetime.strptime(new_analysis_date, '%Y-%m-%dT%H:%M:%S'):
+        return False
+    
+    return True
+
 # Instantiate a DAG with @dag operator
 @dag(
     start_date=datetime(2025, 3, 30),
-    schedule="30 3,15 * * *", # run twice a day at 03.30 UTC and 15.30 UTC
+    schedule="0 1,13 * * *", # run twice a day at 01.00 UTC and 13.00 UTC
     catchup=False,
     tags=["etl", "forecast"],
 )
 def etl_forecast():
+
     # define extract, transform and load task
 
-    @task()
     def extract():
         
         # get forecasts data
         data = get_forecasts()
+        
+        # check if clickhouse client succesfully connect
+        if client is None:
+            return
+
+        # check if the data we got is the newest version
+        if not is_newest_version(data[0]['analysis_date']):
+            raise AirflowException("Data is not the newest version. Retrying...")
 
         return data
+
+    # extract task that will re-run in the next 30 minutes when is_newest_version() return False value
+    extract_task = PythonOperator(
+        task_id="extract_task",
+        python_callable=extract,
+        retries=3,
+        retry_delay=timedelta(minutes=30),
+    )
     
-    @task()
-    def transform(forecasts_data: list):
+    def transform(ti):
+        forecasts_data = ti.xcom_pull(task_ids="extract_task")
 
         # convert datetime column value from string to datetime
         for forecast_data in forecasts_data:
@@ -47,15 +80,17 @@ def etl_forecast():
         data = [list(d.values()) for d in forecasts_data]
 
         return data
+    
+    transform_task = PythonOperator(
+        task_id="transform_task",
+        python_callable=transform,
+        provide_context=True,
+    )
 
     
-    @task()
-    def load(forecast_data: list):
-        
-        # create clickhouse connection
-        client = get_clickhouse_connection()
-        if client is None:
-            return
+    def load(ti):
+        forecast_data = ti.xcom_pull(task_ids="transform_task")
+        print('forecast_data', forecast_data)
         
         # insert data to clickhouse
         client.insert(
@@ -65,12 +100,15 @@ def etl_forecast():
         )
 
         print(f"Insert forecasts data to clickhouse success. Total forecasts data is: {len(forecast_data)}")
-        
+
+    load_task = PythonOperator(
+        task_id="load_task",
+        python_callable=load,
+        provide_context=True,
+    )   
 
     # Main flow of the DAG
-    raw_forecast_data = extract()
-    forecast_data = transform(raw_forecast_data)
-    load(forecast_data)
+    extract_task >> transform_task >> load_task 
 
 
 etl_forecast()
